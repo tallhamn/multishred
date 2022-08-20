f160b94dad0ba1dd8bcf9a0f8640ea6ae1bea0e3
Marcus Tallhamn <marcus@tallhamn.com>
2022-08-20 08:05:06 -0500

    made workers private

--- src/lib.rs
+++ src/lib.rs
@@ -40,14 +40,14 @@ impl ThreadPool{
 }
 
 
-pub struct  Worker{
+struct  Worker{
     id: usize,
     thread: thread::JoinHandle<()>,
 }
 
 impl Worker{
 
-    pub fn new (id: usize) -> Worker{
+    fn new (id: usize) -> Worker{
         let thread = thread::spawn(|| {});
 
         Worker { id, thread }
