48517d9df21f401f6d668ebf5e370bbcaf10fbd7
Marcus Tallhamn <marcus@tallhamn.com>
2022-08-19 20:29:13 -0500

    checking for GET requests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,6 +17,22 @@ fn main() {
 fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get){
+
+        let contents = fs::read_to_string("hello.html").unwrap();
+
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+
+    else {
+        // other request todo
+    }
+    //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
 }
